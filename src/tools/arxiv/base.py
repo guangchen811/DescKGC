89ae5c3f98a5516e241310e50b66a1dffe48f63d
guangchen <guangchen811@gmail.com>
2023-05-25 21:13:42 +0800

    fix bug in arxiv tool
    fix arxiv tool's function: dump_to_json by add a str function for date
    type variable like publishdate and update date.

--- src/tools/arxiv/base.py
+++ src/tools/arxiv/base.py
@@ -8,15 +8,14 @@ def response_to_json(search_res):
         res.append({
             "title": result.title,
             "authors": [author.name for author in result.authors],
-            "published": result.published,
-            "updatedDate": result.updated,
+            "published": str(result.published),
+            "updatedDate": str(result.updated),
             "summary": result.summary,
             "doi": result.doi,
             "primary_category": result.primary_category,
             "categories": result.categories,
             "pdf_url": result.pdf_url,
         })
-    print(result.authors)
     return res
 def dump_to_json(res, dir_path, file_name):
     file_path = dir_path + file_name + '.json'
@@ -28,10 +27,9 @@ if __name__ == '__main__':
     query = "network science"
     search = arxiv.Search(
         query = query,
-        max_results = 1,
+        max_results = 10,
         sort_by = arxiv.SortCriterion.Relevance,
         sort_order = arxiv.SortOrder.Descending
     )
     res = response_to_json(search)
-    print(res)
-    # dump_to_json(res, './data/', query.replace(' ', '_'))
+    dump_to_json(res, './data/', query.replace(' ', '_'))
