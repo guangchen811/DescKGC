4a1300e74832c3c8ccd61ced850d350561e523ac
guangchen <guangchen811@gmail.com>
2023-05-25 21:21:18 +0800

    refactor extractor tool
    refactor triple extractor tool. integrate the init process into
    init_extract_chain

--- main.py
+++ main.py
@@ -1,42 +1,12 @@
 import json
 from langchain.chat_models import ChatOpenAI
-from langchain.prompts.chat import (
-    ChatPromptTemplate,
-    HumanMessagePromptTemplate,
-)
-from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
-from langchain.chains import SequentialChain
-from src.tools.extractor.template import ENTITY_EXTRACT_TEMPLATE, RELATION_EXTRACT_TEMPLATE
+from src.tools.extractor.base import init_extract_chain
 
-with open('./data/arxiv_network_science.json', 'r') as f:
+with open('./data/network_science.json', 'r') as f:
     res = json.load(f)
 
-entity_human_message_prompt = HumanMessagePromptTemplate(
-        prompt=PromptTemplate(
-            template=ENTITY_EXTRACT_TEMPLATE,
-            input_variables=["topic", "summary"],
-        )
-    )
-entity_chat_prompt_template = ChatPromptTemplate.from_messages([entity_human_message_prompt])
-
-relation_human_message_prompt = HumanMessagePromptTemplate(
-        prompt=PromptTemplate(
-            template=RELATION_EXTRACT_TEMPLATE,
-            input_variables=["summary", 'entities'],
-        )
-    )
-relation_chat_prompt_template = ChatPromptTemplate.from_messages([relation_human_message_prompt])
-
-chat = ChatOpenAI(temperature=0.3)
-entity_extract_chain = LLMChain(llm=chat, prompt=entity_chat_prompt_template, output_key="entities")
-relation_extract_chain = LLMChain(llm=chat, prompt=relation_chat_prompt_template, output_key="relations")
-
-extract_chain = SequentialChain(
-    chains=[entity_extract_chain, relation_extract_chain],
-    input_variables=["topic", "summary"],
-    output_variables=["entities", "relations"],
-    verbose=True)
+llm = ChatOpenAI(temperature=0.3)
+extract_chain = init_extract_chain(llm)
 
 for idx, a_res in enumerate(res):
     if idx >=1:
--- src/tools/extractor/base.py
+++ src/tools/extractor/base.py
@@ -2,7 +2,43 @@ import json
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
+from .template import ENTITY_EXTRACT_TEMPLATE, RELATION_EXTRACT_TEMPLATE
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from langchain.chains import SequentialChain
 
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+)
+
+def init_extract_chain(llm):
+    entity_human_message_prompt = HumanMessagePromptTemplate(
+            prompt=PromptTemplate(
+                template=ENTITY_EXTRACT_TEMPLATE,
+                input_variables=["topic", "summary"],
+            )
+        )
+    entity_chat_prompt_template = ChatPromptTemplate.from_messages([entity_human_message_prompt])
+
+    relation_human_message_prompt = HumanMessagePromptTemplate(
+            prompt=PromptTemplate(
+                template=RELATION_EXTRACT_TEMPLATE,
+                input_variables=["summary", 'entities'],
+            )
+        )
+    relation_chat_prompt_template = ChatPromptTemplate.from_messages([relation_human_message_prompt])
+
+    entity_extract_chain = LLMChain(llm=llm, prompt=entity_chat_prompt_template, output_key="entities")
+    relation_extract_chain = LLMChain(llm=llm, prompt=relation_chat_prompt_template, output_key="relations")
+
+    extract_chain = SequentialChain(
+        chains=[entity_extract_chain, relation_extract_chain],
+        input_variables=["topic", "summary"],
+        output_variables=["entities", "relations"],
+        verbose=True
+    )
+    return extract_chain
 
 if __name__ == '__main__':
     from template import ENTITY_EXTRACT_TEMPLATE
