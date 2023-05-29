a8d19730c62f7d1035097cec73c1210f09f22d62
guangchen811 <guangchen811@gmail.com>
2023-05-29 15:24:59 +0800

    modify neo4j tool
    immigrate some isolated functions from base.py to utils.py to make the code easy to follow.
    add a new feature (delete by node type) in neo4j tool.

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 KG auto construction
 
+### 2023-05-29
+
+1. devide author entities from arxiv entities.
+2. add clean function into neo4j module.
+
 ### 2023-05-25
 1. basic arxiv requirement module has been finished.
 2. thinking about how to build a minimal dataset for entity alignment.
--- src/tools/neo4j/base.py
+++ src/tools/neo4j/base.py
@@ -1,23 +1,11 @@
-from langchain.graphs import Neo4jGraph
 from .cypher_template import (
-    ARXIV_PAPER_INSERT_INSTRUCTION
+    ARXIV_PAPER_INSERT_INSTRUCTION,
+    DELETE_NODES_INSTRUCTION
 )
+from .utils import response_to_json, join_if_list
+
+from langchain.graphs import Neo4jGraph
 from tqdm import tqdm
-def response_to_json(search_res):
-    res = []
-    for result in search_res.results():
-        res.append({
-            "title": result.title,
-            "authors": [author.name for author in result.authors],
-            "published": str(result.published),
-            "updatedDate": str(result.updated),
-            "summary": result.summary,
-            "doi": result.doi,
-            "primary_category": result.primary_category,
-            "categories": result.categories,
-            "pdf_url": result.pdf_url,
-        })
-    return res
 
 class Neo4jManager():
     def __init__(self):
@@ -28,6 +16,7 @@ class Neo4jManager():
         )
         self.graph_schema = self.graph.get_schema
         self.arxiv_paper_insert_instruction = ARXIV_PAPER_INSERT_INSTRUCTION
+        self.delete_nodes_instruction = DELETE_NODES_INSTRUCTION
     def add_from_arxiv(self, arxiv_res):
         cypher_insturction_list = [
             self._arxiv_res_to_cypher(res)
@@ -40,28 +29,34 @@ class Neo4jManager():
                 print(f"arxiv paper insert error: {Exception}")
         self.update_schema()
     
-    def _arxiv_res_to_cypher(self, arxiv_dict):
+    def _arxiv_res_to_cypher(self, arxiv_dict: dict) -> str:
         """ input: an arxiv res dict return from response_to_json
             output: a cypher CREATE instruction
         """
         cypher_insturction = self.arxiv_paper_insert_instruction.format(
-            title=self._join_if_list(arxiv_dict["title"]),
-            authors=self._join_if_list(arxiv_dict["authors"]),
-            published=self._join_if_list(arxiv_dict["published"]),
-            updatedDate=self._join_if_list(arxiv_dict["updatedDate"]),
-            summary=self._join_if_list(arxiv_dict["summary"].replace('\n', ' ')),
-            doi=self._join_if_list(arxiv_dict["doi"]),
-            primary_category=self._join_if_list(arxiv_dict["primary_category"]),
-            categories=self._join_if_list(arxiv_dict["categories"]),
-            pdf_url=self._join_if_list(arxiv_dict["pdf_url"])
+            title=join_if_list(arxiv_dict["title"]),
+            authors=join_if_list(arxiv_dict["authors"]),
+            published=join_if_list(arxiv_dict["published"]),
+            updatedDate=join_if_list(arxiv_dict["updatedDate"]),
+            summary=join_if_list(arxiv_dict["summary"].replace('\n', ' ')),
+            doi=join_if_list(arxiv_dict["doi"]),
+            primary_category=join_if_list(arxiv_dict["primary_category"]),
+            categories=join_if_list(arxiv_dict["categories"]),
+            pdf_url=join_if_list(arxiv_dict["pdf_url"])
         )
         return cypher_insturction
-    def _join_if_list(self, input):
-        if isinstance(input, list):
-            return "||".join(input)
-        else:
-            return str(input)
         
-    def update_schema(self):
+    def update_schema(self) -> None:
         self.graph.refresh_schema()
         self.graph_schema = self.graph.get_schema
+
+    def delete_by_type(self, type: str) -> None:
+        """ 
+        delete all nodes of a type.
+        TODO: implement delete by attribute
+        """
+        cypher_insturction = self.delete_nodes_instruction.format(
+            type=type
+        )
+        self.graph.query(cypher_insturction)
+        self.update_schema()
