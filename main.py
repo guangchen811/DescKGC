1d5514c67d34ad8755fe8c05ae6c2b066b4490cc
guangchen811 <guangchen811@gmail.com>
2023-05-29 16:54:38 +0800

    start intergrate extraction block with neo4j
    build an uncomplete framework in main.py

--- main.py
+++ main.py
@@ -3,11 +3,35 @@ from langchain.chat_models import ChatOpenAI
 from src.tools.extractor.base import init_extract_chain
 from src.tools.neo4j.base import Neo4jManager
 
-with open('./data/complex_network.json', 'r') as f:
-    res = json.load(f)
-
 llm = ChatOpenAI(temperature=0.3)
 extract_chain = init_extract_chain(llm)
 
 db_manager = Neo4jManager()
-db_manager.add_from_arxiv(res)
+# print(db_manager.graph_schema)
+
+summary_1 = db_manager.graph.query(
+    "MATCH (n:Paper) WHERE n.doi=$doi RETURN n.summary as summary",
+    params={"doi": '10.1103/PhysRevE.96.042304'}
+)
+
+summary_2 = db_manager.graph.query(
+    "MATCH (n:Paper) WHERE n.doi=$doi RETURN n.summary as summary",
+    params={"doi": '10.1016/j.physleta.2014.02.010'}
+)
+
+def extract_entities_from_paper(doi):
+    summary = db_manager.graph.query(
+        "MATCH (n:Paper) WHERE n.doi=$doi RETURN n.summary as summary",
+        params={"doi": doi}
+    )
+    res = extract_chain({"topic":"network science", "summary":summary}, verbose=False)
+    cypher_instruction = """
+    MATCH (n:Paper) WHERE n.doi='10.1016/j.physleta.2014.02.010'
+    MERGE (x:)
+    RETURN n
+    """
+    # TODO: the instruction above need to be completed.
+    return res
+
+res = extract_chain({"topic":"network science", "summary":summary_2}, verbose=False)
+print(res)
