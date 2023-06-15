22f16284072bc37f3810fb6a5fca041a7a82ce48
guangchen811 <guangchen811@gmail.com>
2023-06-15 16:53:21 +0800

    for loop for similarity computation

--- src/procedure/align_across_subgraphs/base.py
+++ src/procedure/align_across_subgraphs/base.py
@@ -19,7 +19,13 @@ if __name__ == '__main__':
     cur_path = os.path.dirname(__file__)
     id_pair_list = read_json_cases(f'{cur_path}/example_papers.json')
     papers_list, entities_list = parse_paper_id_pairs(id_pair_list, db_manager)
-    cos_metric = cos_metric_between_sent_sets(entities_list[0], entities_list[1], tokenizer, model)
-    print([entity['id'] for entity in entities_list[0]])
-    add_sim_metric_to_db(db_manager, cos_metric, entities_list[0], entities_list[1])
+    for i in range(len(entities_list)):
+        for j in range(i, len(entities_list)):
+            cos_metric = cos_metric_between_sent_sets(entities_list[i], entities_list[j], tokenizer, model)
+            add_sim_metric_to_db(db_manager, cos_metric, entities_list[i], entities_list[j])
+    # cos_metric = cos_metric_between_sent_sets(entities_list[0], entities_list[1], tokenizer, model)
+    # print([entity['id'] for entity in entities_list[0]])
+    # add_sim_metric_to_db(db_manager, cos_metric, entities_list[0], entities_list[1])
+
+    
     db_manager.close_driver()
