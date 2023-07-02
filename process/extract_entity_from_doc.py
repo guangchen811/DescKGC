d2e1dfa83d5d8be6c99d7e93a47447e7d1a7482f
guangchen811 <guangchen811@gmail.com>
2023-07-02 19:42:44 +0800

    add a function to extract multi papers. (need to implement async version

--- process/extract_entity_from_doc.py
+++ process/extract_entity_from_doc.py
@@ -3,11 +3,11 @@ import argparse
 from langchain.chat_models import ChatOpenAI
 from src.tools.neo4j.base import Neo4jManager
 from src.tools.extractor.base import init_extract_chain
-from src.procedure.extract_and_insert.base import extract_entities_from_paper
+from src.procedure.extract_and_insert.base import extract_entities_from_paper, get_paper_title_by_filed
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--id_type', type=str, required=True)
-parser.add_argument('--id_value', type=str, required=True)
+parser.add_argument('--id_type', type=str)
+parser.add_argument('--id_value', type=str)
 args = parser.parse_args()
 
 llm = ChatOpenAI(temperature=0.0)
@@ -16,5 +16,8 @@ db_manager = Neo4jManager()
 
 # print(db_manager.graph_schema)
 
-# title = 'Product of two staircase rank one transformations that is not loosely Bernoulli'
+# title = 'The Sharing Economy for the Smart Grid'
 # extract_entities_from_paper(id_type='title', id_value=title, db_manager=db_manager, extract_chain=extract_chain)
+res = get_paper_title_by_filed(db_manager, 'doi')
+for title in res:
+    extract_entities_from_paper(id_type='title', id_value=title, db_manager=db_manager, extract_chain=extract_chain)
--- src/procedure/extract_and_insert/base.py
+++ src/procedure/extract_and_insert/base.py
@@ -12,3 +12,13 @@ def extract_entities_from_paper(id_type, id_value, db_manager, extract_chain):
         id_dict[entity['name']] = elementid
     for relation_triple in relation_list:
         add_one_relation(db_manager, relation_triple, id_dict)
+
+def extract_entities_from_papers(id_type, id_value_list, db_manager, extract_chain):
+    for id_value in id_value_list:
+        extract_entities_from_paper(id_type, id_value, db_manager, extract_chain)
+
+def get_paper_title_by_filed(db_manager, field_name):
+    # return paper titles who have field_name
+    query = "MATCH (p:Paper) WHERE p.{} IS NOT NULL RETURN p.title AS titles".format(field_name)
+    res = db_manager.graph.query(query)
+    return [r['titles'] for r in res]
