a416d158a486c5b0108e2ed1d4e658ada04740c4
guangchen811 <guangchen811@gmail.com>
2023-07-14 15:16:20 +0800

    using config as temperature provider

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from src.procedure.extract_and_insert.base import extract_entities_from_paper
 with open('./config.yaml', 'r') as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
 
-llm = ChatOpenAI(temperature=0.3)
+llm = ChatOpenAI(temperature=config['llm']['temperature'])
 extract_chain = init_extract_chain(llm)
 
 db_manager = DBManager()
