bb2c10ca4c42ec88b9b6bb20fdd16a3c6c6af5e1
guangchen811 <guangchen811@gmail.com>
2023-07-26 15:37:01 +0800

    refact some scripts

--- scripts/manage_db.py
+++ scripts/manage_db.py
@@ -1,3 +1,5 @@
+# scripts/manage_db.py
+
 import argparse
 import re
 
@@ -5,29 +7,26 @@ import yaml
 
 from src.tools.db_manager.base import DBManager
 
-with open('./config.yaml', 'r') as f:
-    config = yaml.load(f, Loader=yaml.FullLoader)
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--delete-by-type', action='store_true')
-parser.add_argument('--delete-all', action='store_true')
-parser.add_argument('--show-schema', action='store_true')
-args = parser.parse_args()
+def main(args):
+    db_manager = DBManager(**config['neo4jdb'], **config['chromadb'])
+    node_labels = db_manager.get_node_labels()
 
-db_manager = DBManager(**config['neo4jdb'], **config['chromadb'])
-node_labels = db_manager.get_node_labels()
+    if args.delete_by_type:
+        delete_by_type(db_manager, node_labels)
+
+    if args.delete_all:
+        delete_all(db_manager, node_labels)
+
+    if args.show_schema:
+        show_schema(db_manager)
 
 
-if args.delete_by_type:
-    if len(node_labels) == 0:
-        print('No data in the database.')
-    else:
-        type_name = input('The available types are: [' + ', '.join(
-            node_labels) + '].' + '\nPlease input the type name: \n')
-        db_manager.delete_by_type(type_name)
-        print(f'Delete all nodes with type {type_name}.')
+def show_schema(db_manager):
+    db_manager.show_schema()
 
-if args.delete_all:
+
+def delete_all(db_manager, node_labels):
     if len(node_labels) == 0:
         print('No data in the database.')
     else:
@@ -39,5 +38,29 @@ if args.delete_all:
         else:
             print('Delete aborted.')
 
-if args.show_schema:
-    db_manager.show_schema()
+
+def delete_by_type(db_manager, node_labels):
+    if len(node_labels) == 0:
+        print('No data in the database.')
+    else:
+        print('The available types are:', end=' ')
+        print(f'{", ".join(node_labels)}.')
+        type_name = input("Please input the type name: \n")
+        db_manager.delete_by_type(type_name)
+        print(f'Delete all nodes with type {type_name}.')
+
+
+if __name__ == "__main__":
+    with open('./config.yaml', 'r') as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('--delete-by-type', action='store_true',
+                       help="Delete nodes by type. You will be asked to enter the type.")
+    group.add_argument('--delete-all', action='store_true',
+                       help="Delete all nodes. You will be asked to confirm.")
+    group.add_argument('--show-schema', action='store_true',
+                       help="Show the schema of the database.")
+    args = parser.parse_args()
+    main(args)
