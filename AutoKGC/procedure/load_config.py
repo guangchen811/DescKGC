d4317397110aa657bd7a671b3503bf813d3f7747
guangchen811 <guangchen811@gmail.com>
2023-07-27 11:01:40 +0800

    add file checking mechanism in load_config

--- AutoKGC/procedure/load_config.py
+++ AutoKGC/procedure/load_config.py
@@ -1,7 +1,18 @@
+import os
+
 import yaml
 
 
 def load_config():
-    with open('../config.yaml', 'r') as f:
-        config = yaml.load(f, Loader=yaml.FullLoader)
-    return config
+    config_file = './.kgc_config.yaml'
+    if os.path.exists(config_file):
+        with open(config_file, 'r') as f:
+            config = yaml.safe_load(f)
+        return config
+    else:
+        raise FileNotFoundError(f"Configuration file '{config_file}' not found. Please ensure the file exists.")
+
+
+if __name__ == "__main__":
+    config = load_config()
+    print(config)
