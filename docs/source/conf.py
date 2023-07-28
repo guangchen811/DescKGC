5f64d87432ad3a282f7cb502c024cc228c4adaf6
guangchen811 <guangchen811@gmail.com>
2023-07-28 15:46:36 +0800

    release v0.0.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ sys.path.insert(0, os.path.abspath('../../'))
 project = 'AutoKGC'
 copyright = '2023, Hao Chen'
 author = 'Hao Chen'
-release = 'v0.0.1'
+release = 'v0.0.2'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
