c205089ea3407fe17350f926e868e310c63449b4
guangchen811 <guangchen811@gmail.com>
2023-07-30 13:47:31 +0800

    update cli

--- AutoKGC/scripts/cli.py
+++ AutoKGC/scripts/cli.py
@@ -7,23 +7,27 @@ def main():
     parser = argparse.ArgumentParser(description="AutoKGC CLI")
     subparsers = parser.add_subparsers(dest="command")
 
-    module_manage_db = subparsers.add_parser("manage-db", help="")
+    module_manage_db = subparsers.add_parser("manage-db", help="Monitors and manages the database.")
     manage_db.add_arguments(module_manage_db)
     module_manage_db.set_defaults(func=manage_db.main)
 
-    module_search_from_arxiv = subparsers.add_parser("search-from-arxiv", help="")
+    module_search_from_arxiv = subparsers.add_parser(
+        "search-from-arxiv", help="Search papers from arXiv and dump them as json."
+    )
     search_from_arxiv.add_arguments(module_search_from_arxiv)
     module_search_from_arxiv.set_defaults(func=search_from_arxiv.main)
 
-    module_extract_entity_from_doc = subparsers.add_parser("extract-entity-from-doc", help="")
+    module_extract_entity_from_doc = subparsers.add_parser(
+        "extract-entity-from-doc", help="extract entities from documents in the database."
+    )
     extract_entity_from_doc.add_arguments(module_extract_entity_from_doc)
     module_extract_entity_from_doc.set_defaults(func=extract_entity_from_doc.main)
 
-    module_add_from_arxiv = subparsers.add_parser("add-from-arxiv", help="")
+    module_add_from_arxiv = subparsers.add_parser("add-from-arxiv", help="Add papers from local json files to databases.")
     add_from_arxiv.add_arguments(module_add_from_arxiv)
     module_add_from_arxiv.set_defaults(func=add_from_arxiv.main)
 
-    module_entity_alignment = subparsers.add_parser("entity-alignment", help="")
+    module_entity_alignment = subparsers.add_parser("entity-alignment", help="Align entities in the database.")
     entity_alignment.add_arguments(module_entity_alignment)
     module_entity_alignment.set_defaults(func=entity_alignment.main)
 
--- setup.py
+++ setup.py
@@ -17,11 +17,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'autokgc-add-from-arxiv=AutoKGC.scripts.main.add_from_arxiv:main',
-            'autokgc-manage-db=AutoKGC.scripts.manage_db:main',
-            'autokgc-entity-alignment=AutoKGC.scripts.entity_alignment:main',
-            'autokgc-extract-entity-from-doc=AutoKGC.scripts.extract_entity_from_doc:main',
-            'autokgc-search-from-arxiv=AutoKGC.scripts.search_from_arxiv:main',
+            'autokgc=AutoKGC.scripts.cli:main'
         ],
     },
 )
