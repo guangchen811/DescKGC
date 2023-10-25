dd729afb98ea529fb9d2d4a77389e2695fac9252
guangchen811 <guangchen811@gmail.com>
2023-10-25 13:52:59 +0800

    noqa

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,4 @@
+# flake8: noqa
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -6,36 +7,36 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('../../'))
+sys.path.insert(0, os.path.abspath("../../"))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'DescKGC'
-copyright = '2023, Hao Chen'
-author = 'Hao Chen'
-release = 'v0.0.3'
+project = "DescKGC"
+copyright = "2023, Hao Chen"
+author = "Hao Chen"
+release = "v0.0.3"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode"]
 autodoc_default_options = {
     # 'members': None,  # don't include class members
     # 'undoc-members': None,  # don't include members without docstrings
 }
-autodoc_member_order = 'groupwise'
+autodoc_member_order = "groupwise"
 
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'furo'
-html_static_path = ['_static']
+html_theme = "furo"
+html_static_path = ["_static"]
 html_show_sourcelink = True
 html_theme_options = {
     "footer_icons": [
